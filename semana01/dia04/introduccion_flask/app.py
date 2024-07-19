c2963b8dfc8e52a7b2b554d39d87cb22eb07bac4
12aptor <paholo2054@gmail.com>
2024-07-18 22:57:20 -0500

    introducción a flask

--- semana01/dia04/introduccion_flask/app.py
+++ semana01/dia04/introduccion_flask/app.py
@@ -1,5 +1,5 @@
 # Importamos la clase Flask desde el paquete flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 
 # Instanciamos la clase Flask
 app = Flask(__name__)
@@ -35,7 +35,7 @@ def sign_in():
 # Tambien podemos definir una ruta con un método específico @app.post()
 @app.post('/sign-up')
 def sign_up():
-    return 'Registrando usuario'
+    return 'Registrando usuario', 201
 
 # Renderizamos una plantilla HTML
 @app.route('/home')
@@ -47,13 +47,36 @@ def home_page():
 def create_product():
     image = request.files['image']
     print(image)
-    
+
     # El request.form['key'] siempre devuelve un string
     name = request.form['name']
     print(name)
     price = request.form['price']
     print(type(price))
     return 'Creando producto'
+
+@app.get('/secure')
+def secure():
+    # Recuperamos headers
+    # headers = request.headers
+    # jwt = headers['Authorization']
+
+    # Recuperar cookies
+    cookies = request.cookies.get('secure')
+    print(cookies)
+    return 'Página segura'
+
+@app.get('/send-cookies')
+def send_cookies():
+    response = make_response({
+        'message': 'Cookies enviadas'
+    }, 200)
+    response.set_cookie('secure', '123456')
+    return response
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Página no encontrada', 404
 
 # Corremos la aplicación
 if __name__ == '__main__':
