8c9f04867bdff5a8b267b102ed7a3b8f09da4621
12aptor <paholo2054@gmail.com>
2024-07-19 22:33:45 -0500

    flask_boilerplate

--- semana02/flask-boilerplate/app.py
+++ semana02/flask-boilerplate/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.user_router import user_router
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.register_blueprint(user_router, url_prefix='/api/user')
 
