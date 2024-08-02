60a1b2b83f2f3c506e808d0c8fbfc5b47da2bb4c
12aptor <paholo2054@gmail.com>
2024-08-01 21:01:43 -0500

    flask ecommerce crear ordenes

--- semana02/flask-ecommerce/app.py
+++ semana02/flask-ecommerce/app.py
@@ -1,22 +1,15 @@
 from flask import Flask
 from db import db
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from config import Config
+
 from routes.rol_router import rol_router
 from routes.user_router import user_router
 from routes.category_router import category_router
 from routes.product_router import product_router
-from flask_jwt_extended import JWTManager
-from config import Config
+from routes.order_router import order_router
 
-from models import (
-    category_model,
-    order_detail_model,
-    order_model,
-    product_model,
-    rol_model,
-    update_product_log_model,
-    user_model
-)
 
 app = Flask(__name__)
 
@@ -30,6 +23,7 @@ app.register_blueprint(rol_router, url_prefix='/api/rol')
 app.register_blueprint(user_router, url_prefix='/api/user')
 app.register_blueprint(category_router, url_prefix='/api/category')
 app.register_blueprint(product_router, url_prefix='/api/product')
+app.register_blueprint(order_router, url_prefix='/api/order')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- semana02/flask-ecommerce/controllers/order_controller.py
+++ semana02/flask-ecommerce/controllers/order_controller.py
@@ -1,17 +1,44 @@
+from pydantic import ValidationError
 from models.product_model import ProductModel
+from models.order_model import OrderModel
+from models.order_detail_model import OrderDetailModel
 from schemas.order_schema import OrderSchema
 
 
 class OrderController:
     def __init__(self) -> None:
-        self.model = ProductModel
+        self.product_model = ProductModel
+        self.order_model = OrderModel
+        self.order_detail_model = OrderDetailModel
 
     def create(self, json: dict):
         try:
-            # Validar los datos
+            # Validar la data
+            validated_order = OrderSchema(**json)
 
+            # Validar el stock
+            details = validated_order.order_details
+            
+            for detail in details:
+                product = self.product_model.query.get(detail.product_id)
 
-            # Validar el stock del producto
+                # Validar la existencia del producto
+                if product is None:
+                    return {
+                        'message': 'Product not found',
+                    }, 404
+                
+                # Validar el estado del producto
+                if product.status == False:
+                    return {
+                        'message': f'Product {product.name} is out of stock',
+                    }, 400
+                
+                # Validar el stock del producto
+                if product.stock < detail.quantity:
+                    return {
+                        'message': f'Product {product.name} is out of stock'
+                    }, 400
 
             # Crear la vente
 
@@ -20,7 +47,16 @@ class OrderController:
             # Guardar los cambios en la base de datos
 
             return {
-
+                'message': 'Order created successfully',
             }, 201
+        except ValidationError as e:
+            return {
+                'message': 'Validation Error',
+                'errors': e.errors(),
+            }, 400
+
         except Exception as e:
-            pass
+            return {
+                'message': 'An error occurred',
+                'error': str(e)
+            }, 500
