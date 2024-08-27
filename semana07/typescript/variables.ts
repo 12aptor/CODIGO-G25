9db6e53720239207daa5b580ff5df5e4dd9c6868
12aptor <paholo2054@gmail.com>
2024-08-26 22:53:27 -0500

    typescripts

---
+++ semana07/typescript/variables.ts
@@ -0,0 +1,28 @@
+let fullName: string;
+let age: number;
+let alive: boolean;
+let height: number;
+let emptyValue: null;
+let notAssigned: undefined;
+
+fullName = "Eduardo";
+age = 35;
+alive = true;
+height = 1.75;
+emptyValue = null;
+notAssigned = undefined;
+
+let mixed: string | number;
+mixed = "Soy un string";
+mixed = 10;
+
+let numbers: number[];
+numbers = [1, 2, 3];
+
+let letters: string[];
+letters = ["a", "b", "c"];
+
+let person: { id: number; name: string} = { id: 1, name: "Eduardo"};
+
+let married: "YES" | "NO";
+married = "NO";
